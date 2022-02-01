4a40758038a1a7a90eec93d14d61f0c643270d77
alex <alexarroyo9912@gmail.com>
2022-02-01 13:44:58 -0500

    Challenge 5.0

--- game-01/game01.py
+++ game-01/game01.py
@@ -1,22 +1,20 @@
-def firts_sum(my_set, n):
-    set_numbers = "There are no numbers that sums n"
-    for x in my_set:
-        for y in my_set:
-            if x == y:
-                continue
-            if x + y == n:
-                set_numbers = {x, y}
-                return set_numbers
-    return set_numbers
+
+def firts_sum(numbers, n):
+    for i, number in enumerate(numbers):
+        num = n-number
+        if num not in numbers:
+            continue
+        else:
+            return [numbers[i], num]
 
 
 def main():
     input_data = input("Enter set values separted by commas: ")
     input_data_list = input_data.split(",")
-    my_set = {int(x) for x in input_data_list}
+    numbers = [int(x) for x in input_data_list]
     n = int(input("\nEnter a integer: "))
     print("\n")
-    print(firts_sum(my_set, n))
+    print(firts_sum(numbers, n))
 
 
 if __name__ == "__main__":
